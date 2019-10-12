fac93deb391a6885648dc7b1f2626d553d80d9e6
ozarowm <mariusz.ozarowski@gmail.com>
2019-10-12 15:29:16 +0000

    correction

--- test.rb
+++ test.rb
@@ -10,7 +10,7 @@ class MyAppTest < Minitest::Test
 
 	def test_my_default 
 		get '/' 
-		assert_equal 'foo', last_response.body 
+		assert_equal 'Hello, world', last_response.body 
 	end 
 
 	def test_with_params 
